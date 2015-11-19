bfcf2af4f2cf160b3c620e650623cdd40e24e729
agokhale1 <agokhale1@gmail.com>
2015-11-18 19:52:08 -0500

    Added an automatic UpdaterUtil

--- src/me/agokhale1/pvptag/PvPTag.java
+++ src/me/agokhale1/pvptag/PvPTag.java
@@ -4,12 +4,15 @@ import org.bukkit.plugin.java.*;
 
 import me.agokhale1.pvptag.listener.*;
 import me.agokhale1.pvptag.util.*;
+import me.agokhale1.pvptag.util.UpdaterUtil.UpdateType;
 
 public class PvPTag extends JavaPlugin {
 	
 	@Override
 	public void onEnable()
 	{
+		
+		UpdaterUtil updater = new UpdaterUtil(this, 52564, this.getFile(), UpdateType.DEFAULT, true);
 		
 		new TagDataUtil();
 		
